937e3d0bcdca933af882cc6fc029d17133080bc0
akerta <amelia.joulain@gmail.com>
2020-05-29 21:02:06 +0100

    Screen orientation change supported - (onSaveInstanceState() added).

--- app/src/main/java/com/londonappbrewery/destini/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/destini/MainActivity.java
@@ -1,5 +1,6 @@
 package com.londonappbrewery.destini;
 
+import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -23,13 +24,59 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
         // TODO: Step 5 - Wire up the 3 views from the layout to the member variables:
         mStoryTextView = (TextView) findViewById(R.id.storyTextView);
         mButtonTop = (Button) findViewById(R.id.buttonTop);
         mButtonBottom = (Button) findViewById(R.id.buttonBottom);
 
-        final ViewGroup layout = (ViewGroup) mButtonTop.getParent();
+        //final ViewGroup layout = (ViewGroup) mButtonTop.getParent(); //OPTION 1
+        //OPTION 2 - no variable required
+
+        if(savedInstanceState != null) {
+            mStoryIndex = savedInstanceState.getInt("storyIndex");
+
+
+            switch (mStoryIndex) {
+                case 1:
+                    mStoryTextView.setText(R.string.T1_Story);
+                    mButtonTop.setText(R.string.T1_Ans1);
+                    mButtonBottom.setText(R.string.T1_Ans2);
+                    break;
+                case 2:
+                    mStoryTextView.setText(R.string.T2_Story);
+                    mButtonTop.setText(R.string.T2_Ans1);
+                    mButtonBottom.setText(R.string.T2_Ans2);
+                    break;
+                case 3:
+                    mStoryTextView.setText(R.string.T3_Story);
+                    mButtonTop.setText(R.string.T3_Ans1);
+                    mButtonBottom.setText(R.string.T3_Ans2);
+                    break;
+                case 4:
+                    mStoryTextView.setText(R.string.T4_End);
+                    mButtonTop.setVisibility(View.GONE); //OPTION 2
+                    mButtonBottom.setVisibility(View.GONE); //OPTION 2
+                    break;
+                case 5:
+                    mStoryTextView.setText(R.string.T5_End);
+                    mButtonTop.setVisibility(View.GONE); //OPTION 2
+                    mButtonBottom.setVisibility(View.GONE); //OPTION 2
+                    break;
+                case 6:
+                    mStoryTextView.setText(R.string.T6_End);
+                    mButtonTop.setVisibility(View.GONE); //OPTION 2
+                    mButtonBottom.setVisibility(View.GONE); //OPTION 2
+                    break;
+                default:
+                    mStoryIndex = 1;
+                    mStoryTextView.setText(R.string.T1_Story);
+                    mButtonTop.setText(R.string.T1_Ans1);
+                    mButtonBottom.setText(R.string.T1_Ans2);
+            }
+            
+        } else {
+            mStoryIndex = 1;
+        }
 
                 // TODO: Steps 6, 7, & 9 - Set a listener on the top button:
         mButtonTop.setOnClickListener(new View.OnClickListener() {
@@ -42,14 +89,16 @@ public class MainActivity extends AppCompatActivity {
                     mButtonTop.setText(R.string.T3_Ans1);
                     mButtonBottom.setText(R.string.T3_Ans2);
                 } else if (mStoryIndex == 3) {
+                    mStoryIndex = 6;
                     mStoryTextView.setText(R.string.T6_End);
-                    layout.removeView(mButtonTop);
-                    layout.removeView(mButtonBottom);
+                    //layout.removeView(mButtonTop); //OPTION 1
+                    //layout.removeView(mButtonBottom); //OPTION 1
+                    mButtonTop.setVisibility(View.GONE); //OPTION 2
+                    mButtonBottom.setVisibility(View.GONE); //OPTION 2
                 }
                 //note - no default; hard-coded numbers
             }
         });
-
 
         // TODO: Steps 6, 7, & 9 - Set a listener on the bottom button:
         mButtonBottom.setOnClickListener(new View.OnClickListener() {
@@ -62,16 +111,25 @@ public class MainActivity extends AppCompatActivity {
                     mButtonTop.setText(R.string.T2_Ans1);
                     mButtonBottom.setText(R.string.T2_Ans2);
                 } else if (mStoryIndex == 3) {
+                    mStoryIndex = 5;
                     mStoryTextView.setText(R.string.T5_End);
-                    layout.removeView(mButtonTop);
-                    layout.removeView(mButtonBottom);
+                    mButtonTop.setVisibility(View.GONE); //OPTION 2
+                    mButtonBottom.setVisibility(View.GONE); //OPTION 2
                 } else if (mStoryIndex == 2) {
+                    mStoryIndex = 4;
                     mStoryTextView.setText(R.string.T4_End);
-                    layout.removeView(mButtonTop);
-                    layout.removeView(mButtonBottom);
+                    mButtonTop.setVisibility(View.GONE); //OPTION 2
+                    mButtonBottom.setVisibility(View.GONE); //OPTION 2
                 }
                 //note - no default; hard-coded numbers
             }
         });
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        outState.putInt("storyIndex", mStoryIndex);
     }
 }
